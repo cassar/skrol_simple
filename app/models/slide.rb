213bbe20c770c9062e2c745686039ce2b1e717d0
Luke Cassar <luke@tanda.co>
2021-01-10 14:00:12 +1000

    Add some uniqueness and presence validations to slide model

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -1,3 +1,6 @@
 class Slide < ApplicationRecord
   belongs_to :language
+
+  validates :target_script, :english_latin, presence: true
+  validates :target_script, uniqueness: true
 end
--- test/models/slide_test.rb
+++ test/models/slide_test.rb
@@ -4,4 +4,10 @@ class SlideTest < ActiveSupport::TestCase
   test 'associations' do
     assert slides(:one).language == languages(:one)
   end
+
+  test 'validations' do
+    assert_not Slide.new(target_script: slides(:one).target_script).save
+    assert_not slides(:one).update target_script: nil
+    assert_not slides(:one).update english_latin: nil
+  end
 end
